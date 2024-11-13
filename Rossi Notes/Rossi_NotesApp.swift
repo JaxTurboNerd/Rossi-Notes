9e257a91d40e2d8430237c16a6086e01bfd3bb19
Gregory Boyd <gboyd69@yahoo.com>
2024-11-12 23:16:16 -0500

    Initial Commit

---
+++ Rossi Notes/Rossi_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rossi_NotesApp.swift
+//  Rossi Notes
+//
+//  Created by Gregory Boyd on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Rossi_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rossi NotesTests/Rossi_NotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rossi_NotesTests.swift
+//  Rossi NotesTests
+//
+//  Created by Gregory Boyd on 11/12/24.
+//
+
+import Testing
+@testable import Rossi_Notes
+
+struct Rossi_NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
