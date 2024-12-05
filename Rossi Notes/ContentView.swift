07c23fdc8cd5b396dbdb7ab8626816d65d3670cd
Gregory Boyd <gboyd69@yahoo.com>
2024-12-05 15:01:28 -0500

    added basic user authentication for log in.  need to separate the data into Model and ViewModel.

--- Rossi Notes/ContentView.swift
+++ Rossi Notes/ContentView.swift
@@ -14,50 +14,54 @@ struct ContentView: View {
     let appwrite = Appwrite()
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                BackgroundView()
-                VStack{
-                    Spacer()
-                    Text("Protcol Notes")
-                        .font(Font.custom("ConcertOne-Regular", size: 38))
-                    Spacer()
-                    Image("WalkTheDog")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 150)
-                    Text("A collaborative tool to stay updated on dog walking protocols.")
-                        .multilineTextAlignment(.center)
-                        .font(Font.custom("Urbanist-Medium", size: 20))
-                        .padding()
-                    Divider()
-                        .frame(height: 2)
-                        .overlay(.gray)
-                    Spacer()
-                    VStack {
-                        Button{
-                            //action:
-                            isShowingSignIn = true
-                            Task {
-                                let user: () = await appwrite.getAccount()
-                               print(user)
+        if isLoggedIn {
+            HomeTabView(isLoggedIn: $isLoggedIn)
+        } else {
+            NavigationStack {
+                ZStack {
+                    BackgroundView()
+                    VStack{
+                        Spacer()
+                        Text("Protcol Notes")
+                            .font(Font.custom("ConcertOne-Regular", size: 38))
+                        Spacer()
+                        Image("WalkTheDog")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 150)
+                        Text("A collaborative tool to stay updated on dog walking protocols.")
+                            .multilineTextAlignment(.center)
+                            .font(Font.custom("Urbanist-Medium", size: 20))
+                            .padding()
+                        Divider()
+                            .frame(height: 2)
+                            .overlay(.gray)
+                        Spacer()
+                        VStack {
+                            Button{
+                                //action:
+                                isShowingSignIn = true
+                                Task {
+                                    let user: () = await appwrite.getAccount()
+                                   print(user)
+                                }
+                                
+                            } label: {
+                                Text("Sign In")
+                                    .frame(maxWidth: 250)
+                                    .font(.headline)
                             }
-                            
-                        } label: {
-                            Text("Sign In")
-                                .frame(maxWidth: 250)
-                                .font(.headline)
+                            .padding()
+                            .buttonStyle(.borderedProminent)
+                            .controlSize(.large)
                         }
-                        .padding()
-                        .buttonStyle(.borderedProminent)
-                        .controlSize(.large)
+                        .navigationDestination(isPresented: $isShowingSignIn, destination: {SignIn(isLoggedIn: $isLoggedIn)})
+                        Spacer()
                     }
-                    .navigationDestination(isPresented: $isShowingSignIn, destination: {SignIn(isLoggedIn: $isLoggedIn)})
-                    Spacer()
+                    .padding()
                 }
-                .padding()
+                .navigationBarBackButtonHidden()
             }
-            .navigationBarBackButtonHidden()
         }
     }
 }
