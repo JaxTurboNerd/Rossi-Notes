24ba2563054360ba633a3efb0fe6ffcf1c13e1fb
Gregory Boyd <gboyd69@yahoo.com>
2025-01-28 15:14:11 -0500

    moved the components folder under the view folder

---
+++ Rossi Notes/Views/Components/CardView.swift
@@ -0,0 +1,28 @@
+//
+//  CardView.swift
+//  Rossi Notes
+//
+//  Created by Gregory Boyd on 11/16/24.
+//
+
+import SwiftUI
+import Appwrite
+import JSONCodable
+
+struct CardView: View {
+    var name: String = ""
+    
+    var body: some View {
+        Text(name)
+            .font(Font.custom("ConcertOne-Regular", size: 24))
+            .tracking(2)//adds letter spacing
+            .frame(maxWidth: .infinity, minHeight: 60, maxHeight:70 ,alignment: .center)
+            .background(Color("AppBlue"))
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+//#Preview {
+//    CardView()
+//}
---
+++ Rossi Notes/Views/Components/DetailView.swift
@@ -0,0 +1,102 @@
+//
+//  DetailView.swift
+//  Rossi Notes
+//
+//  Created by Gregory Boyd on 12/28/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @StateObject private var viewModel = DetailViewModel()
+    @State private var showUpdateForm = false
+    
+    var collectionId = ""
+    var documentId = ""
+    @Binding var triggerRefresh: Bool
+    @State private var noteDeleted = false
+    //Used to dismiss the form:
+    @Environment(\.dismiss) private var dismiss
+
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                NameBackgroundView()
+                Group {
+                    if viewModel.isLoading {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle())
+                            .controlSize(.large)
+                    } else if let error = viewModel.errorMessage {
+                        VStack {
+                            Text("Error: \(error)")
+                            Button("Retry") {
+                                viewModel.fetchDocument(collectionId: collectionId, documentId: documentId)
+                            }
+                        }
+                    } else {
+                        VStack {
+                            VStack{
+                                //Name:
+                                NameView(name: viewModel.detailsData.name)
+                                //Date:
+                                Text("Protocol Date: \(viewModel.detailsData.protocolDate)")
+                                    .font(.system(size: 20))
+                                    .fontWeight(.bold)
+                                    .padding(.vertical)
+                                //Details:
+                                DetailGroupView(viewModel: viewModel)
+                            }
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1)
+                                    .shadow(color: Color.black.opacity(0.4), radius: 2, x: 2, y: 2)
+                            )
+                            .background(Color("BackgroundMain"))
+                            Spacer()
+                        }
+                    }
+                }
+                .padding()//adds padding to the outer-most view
+                .onAppear{
+                    viewModel.fetchDocument(collectionId: collectionId, documentId: documentId)
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing,
+                        content: {
+                Button("Edit Note"){
+                    showUpdateForm = true
+                }
+                //Displays the update form:
+                .sheet(isPresented: $showUpdateForm, content: {Text("Update From in work!")})
+            })
+            ToolbarItem(placement: .topBarTrailing,
+                        content: {
+                Button("Delete"){
+                    Task {
+                        viewModel.deleteNote(collectionId: collectionId, documentId: documentId)
+                        noteDeleted = true
+                        dismiss.callAsFunction()
+                    }
+                }
+                .foregroundStyle(Color.red)
+            })
+        }
+        .onDisappear{
+            if noteDeleted {
+                triggerRefresh = true
+            }
+        }
+    }
+}
+
+struct NameBackgroundView: View {
+    
+    var body: some View {
+        Color("NameBackground")//custom color with dark mode support
+            .ignoresSafeArea(.all)
+    }
+}
