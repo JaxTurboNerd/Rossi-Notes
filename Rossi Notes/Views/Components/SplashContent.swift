0e743f197cf267ce0f5869922acf98558b6f5b5f
Gregory Boyd <gboyd69@yahoo.com>
2025-03-09 17:37:03 -0400

    Added animations to the splash screen image

--- Rossi Notes/Views/Components/SplashContent.swift
+++ Rossi Notes/Views/Components/SplashContent.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
+enum AnimationPhase: CaseIterable {
+    case beginning, middle, end
+    
+    var opacity: Double {
+        switch self {
+        case .beginning: 1.0
+        case .middle: 0.5
+        case .end: 1.0
+        }
+    }
+    
+    var scale: Double {
+        switch self {
+        case .beginning, .end: 0.8
+        case .middle: 1.2
+        }
+    }
+    
+    var animation: Animation {
+        switch self {
+        case .beginning, .end: .bouncy(duration: 0.5)
+        case .middle: .easeInOut(duration: 1.0)
+        }
+    }
+}
+
 struct SplashContent: View {
     
     @StateObject var user: Appwrite
-    @State var shouldDisplaySplashView = true
+    @State private var shouldDisplaySplashView = true
     
     var body: some View {
         ZStack {
@@ -23,9 +49,19 @@ struct SplashContent: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .padding()
+                    .phaseAnimator(AnimationPhase.allCases) { content, phase in
+                        content
+                            .opacity(phase.opacity)
+                            .scaleEffect(phase.scale)
+                    } animation: { phase in
+                        phase.animation
+                    }
                 Spacer()
             }
             .padding()
+            .onAppear {
+                
+            }
         }
     }
 }
