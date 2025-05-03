876834b1e42ada0dfe82a10cb9f2073e04a896d1
Gregory Boyd <gboyd69@yahoo.com>
2025-05-03 17:52:09 -0400

    removed unused comment from Details VM

--- Rossi Notes/ViewModels/DetailViewModel.swift
+++ Rossi Notes/ViewModels/DetailViewModel.swift
@@ -29,7 +29,6 @@ class DetailViewModel: ObservableObject {
     
     init(appwrite: Appwrite){
         self.appwrite = appwrite
-        //call function to get/set user's initials:
     }
     
     //This function is intended to inject an instance of the DetailsModel:
